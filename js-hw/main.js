bf83306c5f4da79e14ce35249abb82aa92988fc6
Burito739 <vmashkin739@gmail.com>
2025-11-30 15:55:05 +0500

    Add files via upload

---
+++ js-hw/main.js
@@ -0,0 +1,18 @@
+const firstName = "john";
+
+const lastName = "star";
+
+const isStudent = true;
+
+const age = 25;
+
+const currentYear = 2025;
+
+const birthYear = (currentYear - age);
+
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}.`);
+
+
+
+result = 123456789falsetrue
+
